0ef88c4e71ee816c33ea89db1c105fde40e43062
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-25 15:05:57 +0200

    init

---
+++ 01-Recursion - Lab/Recursive Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
