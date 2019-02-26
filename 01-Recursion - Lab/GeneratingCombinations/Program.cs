a7bd11827b69cf6416ea86bf853a48a2fc55fbdc
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-26 09:45:21 +0200

    init

---
+++ 01-Recursion - Lab/GeneratingCombinations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace GeneratingCombinations
+{
+	/// <summary>
+	/// Generate all n choose k combinations. 
+	/// </summary>
+	internal class Program
+    {
+	    private static void Main()
+        {
+	        var elements = ReadElements();
+
+	        var n = int.Parse(Console.ReadLine());
+
+	        if(n < 0)
+	        {
+		        return;
+	        }
+
+	        var combination = new int[ n ];
+	        GenerateCombination(combination, elements, 0, 0);
+		}
+
+	    private static int[] ReadElements() =>
+		    Console.ReadLine()?
+			    .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+			    .Select(int.Parse)
+			    .ToArray();
+
+	    private static void GenerateCombination(int[] combination, int[] elements, int index, int border)
+	    {
+		    if (index == combination.Length)
+		    {
+				Console.WriteLine(string.Join(" ", combination));
+			}
+		    else
+		    {
+			    for (var i = border; i < elements.Length; i++)
+			    {
+				    combination[index] = elements[i];
+				    GenerateCombination(combination, elements, index + 1, i + 1);
+				}
+		    }
+	    }
+	}
+}
--- 01-Recursion - Lab/Recursive Factorial/Program.cs
+++ 01-Recursion - Lab/Recursive Factorial/Program.cs
@@ -2,11 +2,23 @@
 
 namespace Recursive_Factorial
 {
-    class Program
+	/// <summary>
+	/// Program that finds the factorial of a given number by recursion.
+	/// </summary>
+	/// <remarks>
+	/// In practice recursion should not be used here (instead use an iterative solution), this is just an exercise.
+	/// </remarks>
+	internal class Program
     {
-        static void Main(string[] args)
+	    private static void Main()
         {
-            Console.WriteLine("Hello World!");
-        }
+	        var n = int.Parse(Console.ReadLine());
+
+	        var factorial = Factorial(n);
+	        Console.WriteLine(factorial);
+		}
+
+	    private static long Factorial(int n) =>
+		    n == 0 ? 1 : n * Factorial(n - 1);
     }
 }
