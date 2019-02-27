475b111cc9c9d35c08294f53dfec970f34f6808d
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-27 17:17:17 +0200

    init

---
+++ 01-Recursion - Exercises/Combinations with Repetition/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Combinations_with_Repetition
+{
+	/// <summary>
+	/// Recursive program for generating and printing all combinations with duplicates of k elements from a set of n elements (k less or = n).
+	/// In combinations, the order of elements doesn’t matter, therefore (1 2) and (2 1) are the same combination, meaning that once
+	/// you print/obtain (1 2), (2 1) is no longer valid.
+	/// </summary>
+	internal class Program
+    {
+	    private static int[] _arr;
+		private static void Main()
+        {
+	        var setCount = int.Parse(Console.ReadLine());
+	        var loopsCount = int.Parse(Console.ReadLine());
+	        _arr = new int[ loopsCount ];
+	        Combinations(setCount);
+		}
+
+		private static void Combinations(int setCount, int index = 0, int element = 1)
+		{
+			if (index >= _arr.Length)
+			{
+				Print(_arr);
+				return;
+			}
+
+			for (var i = element; i <= setCount; i++)
+			{
+				_arr[index] = i;
+				Combinations(setCount, index + 1, i);
+			}
+		}
+
+		private static void Print(int[] arr) =>
+			Console.WriteLine(string.Join(" ", arr));
+    }
+}
---
+++ 01-Recursion - Exercises/Nested Loops To Recursion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nested_Loops_To_Recursion
+{
+	/// <summary>
+	/// Program that simulates the execution of n nested loops from 1 to n which prints the values of all its iteration
+	/// variables at any given time on a single line. Used recursion.
+	/// </summary>
+	internal class Program
+    {
+	    private static int[] _loops;
+
+		private static void Main()
+        {
+	        var loopsCount = int.Parse(Console.ReadLine());
+	        _loops = new int[ loopsCount ];
+	        NestedLoops( 0, loopsCount);
+		}
+
+		private static void NestedLoops(int currentLoop, int loopsCount)
+		{
+			if(currentLoop == loopsCount)
+			{
+				Print(_loops);
+				return;
+			}
+
+			for (var counter = 1; counter < loopsCount; counter++)
+			{
+				_loops[currentLoop] = counter;
+				NestedLoops(currentLoop + 1, loopsCount);
+			}
+		}
+
+		private static void Print(IEnumerable<int> loops) =>
+			Console.WriteLine(string.Join(" ", loops));
+
+    }
+}
