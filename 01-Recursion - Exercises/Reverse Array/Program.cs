600f024d02578c83a0e6d992929f5f28ccf5cd56
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-27 13:48:49 +0200

    init

---
+++ 01-Recursion - Exercises/Reverse Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_Array
+{
+	/// <summary>
+	/// A program that reverses and prints an array. Used recursion.
+	/// </summary>
+	internal class Program
+    {
+	    private static void Main()
+        {
+	        var arr = Console.ReadLine()?
+		        .Split()
+		        .Select(int.Parse)
+		        .ToArray();
+
+	        Reverse(arr, 0, arr.Length - 1);
+
+	        Console.WriteLine(string.Join(" ", arr));
+		}
+
+	    private static void Reverse(int[] arr, int start, int end)
+	    {
+		    if (start >= end)
+		    {
+			    return;
+		    }
+
+		    var temp = arr[start];
+		    arr[start] = arr[end];
+		    arr[end] = temp;
+
+			Reverse(arr, start + 1, end - 1);
+	    }
+    }
+}
