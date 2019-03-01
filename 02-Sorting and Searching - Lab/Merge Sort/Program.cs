948265741a8b260a53b092f1c6ed4e204ef04807
Jordan Jordanov <d_yugioh@abv.bg>
2019-03-01 09:12:32 +0200

    init

---
+++ 02-Sorting and Searching - Lab/Merge Sort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Merge_Sort
+{
+	/// <summary>
+	/// Sort an array of elements using the famous merge sort.
+	/// </summary>
+	internal class Program
+    {
+        private static void Main()
+        {
+	        var input = Console.ReadLine();
+
+	        if(input == string.Empty)
+	        {
+		        return;
+	        }
+
+	        var arr = input
+		        .Split()
+		        .Select(int.Parse)
+		        .ToArray();
+
+	        MergeSort<int>.Sort(arr);
+
+	        Console.WriteLine(string.Join(" ", arr));
+		}
+    }
+}
