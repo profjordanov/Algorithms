906fd40c40b762737951479aed3cb3ec44634f23
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-24 21:34:32 +0200

    Add files via upload

---
+++ Softuniada/2018/1. Awesome Numbers_Authors Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Softuniada
+{
+    class AwesomeNumbers
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int favouriteNumber = int.Parse(Console.ReadLine());
+
+            var isOdd = number % 2 != 0;
+            var isNegative = number < 0;
+            var isDivisible = number % favouriteNumber == 0;
+
+            if (!isOdd && !isNegative && !isDivisible)
+            {
+                Console.WriteLine("boring");
+            }
+
+            if ((isOdd && !isNegative && !isDivisible) ||
+                (!isOdd && isNegative && !isDivisible) ||
+                (!isOdd && !isNegative && isDivisible))
+            {
+                Console.WriteLine("awesome");
+            }
+
+            if ((isOdd && isNegative && !isDivisible) ||
+                (isOdd && !isNegative && isDivisible) ||
+                (!isOdd && isNegative && isDivisible))
+            {
+                Console.WriteLine("super awesome");
+            }
+
+            if (isOdd && isNegative && isDivisible)
+            {
+                Console.WriteLine("super special awesome");
+            }
+
+        }
+    }
+}
